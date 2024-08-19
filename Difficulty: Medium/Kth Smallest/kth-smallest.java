458bacd178a1c84f59d37c62a4218cc70d2d9729
kowshik jallipalli <119750794+kowshikjallipalli0105@users.noreply.github.com>
2024-08-19 21:33:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Kth Smallest/kth-smallest.java
@@ -0,0 +1,91 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+
+        int t = Integer.parseInt(in.readLine().trim());
+        while (t-- > 0) {
+            String line = in.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            int key = Integer.parseInt(in.readLine().trim());
+            Solution ob = new Solution();
+            out.println(ob.kthSmallest(arr, key));
+        }
+        out.flush();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public static int kthSmallest(int[] arr, int k) {
+        // Your code here
+        int n=arr.length;
+        mergesort(arr,0,n-1);
+        return arr[k-1];
+    }
+    public static void mergesort(int[] arr ,int low,int high){
+        if(low>=high){
+            return;
+        }
+        int mid=(low+high)/2;
+        mergesort(arr,low,mid);
+        mergesort(arr,mid+1,high);
+        merge(arr,low,mid,high);
+        
+    }
+    public static void merge(int[] arr,int low,int mid,int high){
+        int left=low;
+        int right=mid+1;
+        ArrayList<Integer>temp=new ArrayList<>();
+        while(left<=mid && right<=high){
+            if(arr[left]<=arr[right]){
+                temp.add(arr[left]);
+                left++;
+            }
+            else{
+                temp.add(arr[right]);
+                right++;
+            }
+            
+            
+        }
+        while(left<=mid){
+            temp.add(arr[left]);
+            left++;
+        }
+        while(right<=high){
+            temp.add(arr[right]);
+            right++;
+        }
+        for( int i=low;i<=high;i++){
+            arr[i]=temp.get(i-low);
+        }
+        
+        
+    }
+}
