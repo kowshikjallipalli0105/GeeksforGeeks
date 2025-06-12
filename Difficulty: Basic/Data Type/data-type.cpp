973db5c9b641c4657eab2cdce78571ec70559db6
kowshik jallipalli <119750794+kowshikjallipalli0105@users.noreply.github.com>
2025-06-12 19:00:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Data Type/data-type.cpp
+++ Difficulty: Basic/Data Type/data-type.cpp
@@ -1,14 +1,3 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-// {Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
-
-
-// } Driver Code Ends
 // User function Template for C++
 
 class Solution {
@@ -33,21 +22,3 @@ class Solution {
         return -1;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        string str;
-        cin >> str;
-        Solution ob;
-        cout << ob.dataTypeSize(str);
-        cout << "\n";
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
