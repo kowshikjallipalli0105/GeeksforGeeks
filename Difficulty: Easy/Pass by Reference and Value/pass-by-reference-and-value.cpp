9c7f3fa324ca91ea959f695bec5fcd09796421e1
kowshik jallipalli <119750794+kowshikjallipalli0105@users.noreply.github.com>
2025-06-12 20:48:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Pass by Reference and Value/pass-by-reference-and-value.cpp
+++ Difficulty: Easy/Pass by Reference and Value/pass-by-reference-and-value.cpp
@@ -1,11 +1,3 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 // User function Template for C++
 
 class Solution {
@@ -18,22 +10,3 @@ class Solution {
         return res;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int a, b;
-        cin >> a >> b;
-        Solution obj;
-        vector<int> ans = obj.passedBy(a, b);
-        cout << ans[0] << " " << ans[1] << "\n";
-    
-cout << "~" << "\n";
-}
-    return 0;
-}
-
-// } Driver Code Ends
