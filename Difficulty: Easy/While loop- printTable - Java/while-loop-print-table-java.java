60940b2ead284bdd0655bbbe80b0c58b78ee75f3
kowshik jallipalli <119750794+kowshikjallipalli0105@users.noreply.github.com>
2025-06-12 20:46:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/While loop- printTable - Java/while-loop-print-table-java.java
+++ Difficulty: Easy/While loop- printTable - Java/while-loop-print-table-java.java
@@ -1,21 +1,10 @@
-//{ Driver Code Starts
-
-// } Driver Code Ends
-//Back-end complete function Template for Java
-import java.util.*;
-
-public class Main {
-    public static void main(String args[]) {
-        // Your Code Here
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int res=10*n;
-        while(res>=n){
-            System.out.print(res+" ");
-            res-=n;
+class Solution {
+    public void calculateMultiples(int n) {
+        // code here
+        int nn=10;
+        while(nn>0){
+            System.out.print(n*nn+" ");
+            nn--;
         }
     }
 }
-
-//{ Driver Code Starts.
-// } Driver Code Ends
