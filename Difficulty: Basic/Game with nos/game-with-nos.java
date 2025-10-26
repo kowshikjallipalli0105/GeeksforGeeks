5ed7d4d3cf6f8cd48f27009d30ca5c06ca4c61a2
kowshik jallipalli <119750794+kowshikjallipalli0105@users.noreply.github.com>
2025-10-26 12:07:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Game with nos/game-with-nos.java
@@ -0,0 +1,13 @@
+// User function Template for Java
+
+class Complete {
+
+    // Function for finding maximum and value pair
+    public static int[] game_with_number(int arr[], int n) {
+        // Complete the function
+        for(int i=1;i<n;i++){
+            arr[i-1]= arr[i-1]^arr[i];
+        }
+        return arr;
+    }
+}
