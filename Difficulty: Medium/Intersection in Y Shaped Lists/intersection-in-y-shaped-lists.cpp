27fd3fac494156e4f4d08dc571e26e9531fefd81
kowshik jallipalli <119750794+kowshikjallipalli0105@users.noreply.github.com>
2026-01-01 21:35:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Intersection in Y Shaped Lists/intersection-in-y-shaped-lists.cpp
@@ -0,0 +1,29 @@
+/*
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    Node* intersectPoint(Node* head1, Node* head2) {
+        //  Code Here
+        if(head1 ==NULL||head2==NULL){
+            return NULL;
+        }
+        Node *x1 = head1;
+        Node *x2 = head2;
+        while(x1!=x2){
+            x1=(x1==NULL)?head2:x1->next;
+            x2=(x2==NULL)?head1:x2->next;
+        }
+        return x1;
+    }
+};
